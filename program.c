15883097061a969e57a3f22c74792d77f05149c0
Andrei Dobre <andrei.dobr3@gmail.com>
2023-11-13 19:35:12 +0200

    Added functions to open directories and create output files

--- program.c
+++ program.c
@@ -5,37 +5,49 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdint.h>
+#include <dirent.h>
 
 #define FILE_OUT "statistica.txt"
 #define BUFF_SIZE 4
 
-int main(int argc, char** argv)
+DIR* openDir(char* path)
 {
-    if(argc != 2 ) // only one parameter allowed
-    {
-        perror("Usage ./program <input_file>");
-        exit(errno);
-    }
-
-    int fileinp = open(argv[1], O_RDONLY); //opens file in read only mode
-
-    if(fileinp == -1) //if file cannot be opened
+    DIR* dIn = opendir(path);
+    if (dIn == NULL)
     {
         char* errstr = "\0"; 
-        sprintf(errstr, "Could not open file named %s\n", argv[1]); 
+        sprintf(errstr, "Could not open directory named %s\n", path); 
         perror(errstr); //prints an error message stating what file cannot be opened
         exit(errno); //stops the program
     }
 
-    int fileout = creat(FILE_OUT, S_IRWXU); //creates output file
+    return dIn;
+}
 
-    if(fileout == -1) //if output file cannot be created
+int createFile(char* path)
+{
+    int out = creat(path, S_IRWXU); //creates output file
+
+    if(out == -1) //if output file cannot be created
     {
         perror("Could not create \'statistica.txt\' output file\n"); //prints an error message
         exit(errno); //stops the program
     }
 
+    return out;
+}
 
+int main(int argc, char** argv)
+{
+    if(argc != 2 ) // only one parameter allowed
+    {
+        perror("Usage ./program <input_dir>");
+        exit(errno);
+    }
+
+    DIR* dirIn = openDir(argv[1]);
+
+    int fileout = createFile(FILE_OUT);
 
     return 0;
 }
